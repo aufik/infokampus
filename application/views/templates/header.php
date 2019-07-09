4a4f4fb889e979c47049c20237dc240c754c178f
Unknown <suryapermanaputra@students.usu.ac.id>
2019-07-09 19:03:03 +0700

    Tahap Awal Infokampus

---
+++ application/views/templates/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>INFOKAMPUS - Media Informasi Lomba / Beasiswa / Acara</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,,500,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/animate.css">
+    
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/aos.css">
+
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/flaticon.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/icomoon.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/frontoffice/css/style.css">
+  </head>
+  <body>
---
+++ application/views/templates/subfooter.php
@@ -0,0 +1,44 @@
+<!-- Jquery Core Js -->
+    <script src="<?=base_url()?>assets/backoffice/plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="<?=base_url()?>assets/backoffice/plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="<?=base_url()?>assets/backoffice/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="<?=base_url()?>assets/backoffice/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="<?=base_url()?>assets/backoffice/plugins/node-waves/waves.js"></script>
+
+    <!-- Jquery CountTo Plugin Js -->
+    <script src="<?=base_url()?>assets/backoffice/plugins/jquery-countto/jquery.countTo.js"></script>
+
+    <!-- Morris Plugin Js -->
+    <script src="<?=base_url()?>assets/backoffice/plugins/raphael/raphael.min.js"></script>
+    <script src="<?=base_url()?>assets/backoffice/plugins/morrisjs/morris.js"></script>
+
+    <!-- ChartJs -->
+    <script src="<?=base_url()?>assets/backoffice/plugins/chartjs/Chart.bundle.js"></script>
+
+    <!-- Flot Charts Plugin Js -->
+    <script src="<?=base_url()?>assets/backoffice/plugins/flot-charts/jquery.flot.js"></script>
+    <script src="<?=base_url()?>assets/backoffice/plugins/flot-charts/jquery.flot.resize.js"></script>
+    <script src="<?=base_url()?>assets/backoffice/plugins/flot-charts/jquery.flot.pie.js"></script>
+    <script src="<?=base_url()?>assets/backoffice/plugins/flot-charts/jquery.flot.categories.js"></script>
+    <script src="<?=base_url()?>assets/backoffice/plugins/flot-charts/jquery.flot.time.js"></script>
+
+    <!-- Sparkline Chart Plugin Js -->
+    <script src="<?=base_url()?>assets/backoffice/plugins/jquery-sparkline/jquery.sparkline.js"></script>
+
+    <!-- Custom Js -->
+    <script src="<?=base_url()?>assets/backoffice/js/admin.js"></script>
+    <script src="<?=base_url()?>assets/backoffice/js/pages/index.js"></script>
+
+    <!-- Demo Js -->
+    <script src="<?=base_url()?>assets/backoffice/js/demo.js"></script>
+</body>
+
+</html>
