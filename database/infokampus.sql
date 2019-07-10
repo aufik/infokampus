765ee080f7e665a267233a7a3864b1cc0871b1eb
kamil <taufikkamil522@gmail.com>
2019-07-10 18:04:18 +0700

    tambahan database

--- database/infokampus.sql
+++ database/infokampus.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2019 at 02:25 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.10
+-- Generation Time: Jul 10, 2019 at 01:03 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,12 +36,24 @@ CREATE TABLE `lomba` (
   `lomba_link` text NOT NULL,
   `lomba_deadline` datetime NOT NULL,
   `lomba_tiket` enum('Gratis','Bayar') NOT NULL,
-  `lomba_created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `lomba_update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lomba_created_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `lomba_update_time` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `lomba_kat_id` int(11) NOT NULL,
   `lomba_created_by_id` int(11) NOT NULL,
-  `lomba_update_by_id` int(11) NOT NULL
+  `lomba_update_by_id` int(11) NOT NULL,
+  `status` enum('Menunggu','Diterima','Ditolak') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lomba`
+--
+
+INSERT INTO `lomba` (`lomba_id`, `lomba_judul`, `lomba_gambar`, `lomba_isi`, `lomba_link`, `lomba_deadline`, `lomba_tiket`, `lomba_created_time`, `lomba_update_time`, `lomba_kat_id`, `lomba_created_by_id`, `lomba_update_by_id`, `status`) VALUES
+(1, '1', '1', '1', '1', '2019-07-09 00:00:18', 'Gratis', '2019-07-09 21:04:25', '2019-07-17 00:00:00', 1, 1, 1, 'Diterima'),
+(2, '', '', '', '', '0000-00-00 00:00:00', 'Gratis', '2019-07-10 15:32:18', '2019-07-10 15:32:18', 0, 0, 0, 'Diterima'),
+(3, '123', '', '', '', '0000-00-00 00:00:00', 'Gratis', '2019-07-10 15:53:41', '2019-07-10 15:53:41', 0, 0, 0, 'Diterima'),
+(4, '123', '', '', '', '0000-00-00 00:00:00', 'Gratis', '2019-07-10 15:54:33', '2019-07-10 15:54:33', 0, 0, 0, 'Diterima'),
+(5, '123', '', '', '', '0000-00-00 00:00:00', 'Gratis', '2019-07-10 15:55:52', '2019-07-10 17:12:14', 1, 0, 0, 'Menunggu');
 
 -- --------------------------------------------------------
 
@@ -53,6 +65,13 @@ CREATE TABLE `lomba_kategori` (
   `lomba_kategori_id` int(11) NOT NULL,
   `lomba_kategori_nama` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lomba_kategori`
+--
+
+INSERT INTO `lomba_kategori` (`lomba_kategori_id`, `lomba_kategori_nama`) VALUES
+(1, 'Nasional');
 
 -- --------------------------------------------------------
 
@@ -98,7 +117,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `user_name`, `user_pass`, `user_pass_ret`, `user_level`) VALUES
-(1, 'admin', 'admin', 'admin', 'admin');
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -126,7 +145,8 @@ ALTER TABLE `team`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -136,13 +156,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `lomba`
 --
 ALTER TABLE `lomba`
-  MODIFY `lomba_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `lomba_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `lomba_kategori`
 --
 ALTER TABLE `lomba_kategori`
-  MODIFY `lomba_kategori_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `lomba_kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `team`
