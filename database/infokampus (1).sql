21b21a3cc9d898e8d01ed17a82f3c5764b860cc2
kamil <taufikkamil522@gmail.com>
2019-07-14 14:35:07 +0700

    database

--- database/infokampus (1).sql
+++ database/infokampus (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 10, 2019 at 01:03 PM
+-- Generation Time: Jul 14, 2019 at 09:34 AM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
@@ -25,6 +25,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `biodata`
+--
+
+CREATE TABLE `biodata` (
+  `biodata_id` int(11) NOT NULL,
+  `biodata_nama` varchar(255) NOT NULL,
+  `biodata_email` varchar(255) NOT NULL,
+  `biodata_hp` varchar(13) NOT NULL,
+  `biodata_foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kontak`
+--
+
+CREATE TABLE `kontak` (
+  `kontak_id` int(11) NOT NULL,
+  `kontak_nama` varchar(255) NOT NULL,
+  `kontak_email` varchar(255) NOT NULL,
+  `kontak_hp` varchar(13) NOT NULL,
+  `kontak_pesan` text NOT NULL,
+  `kontak_date` datetime NOT NULL,
+  `kontak_status` enum('Belum Dibaca','Sudah Dibaca') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kontak`
+--
+
+INSERT INTO `kontak` (`kontak_id`, `kontak_nama`, `kontak_email`, `kontak_hp`, `kontak_pesan`, `kontak_date`, `kontak_status`) VALUES
+(1, 'test', 'taufikkamil522@gmail.com', '082258515288', 'halo salam kenal', '2019-07-18 10:19:36', 'Sudah Dibaca'),
+(2, '', '', '', '', '2019-07-10 00:00:00', 'Belum Dibaca');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `lomba`
 --
 
@@ -36,6 +74,7 @@ CREATE TABLE `lomba` (
   `lomba_link` text NOT NULL,
   `lomba_deadline` datetime NOT NULL,
   `lomba_tiket` enum('Gratis','Bayar') NOT NULL,
+  `lomba_alamat` varchar(255) NOT NULL,
   `lomba_created_time` datetime NOT NULL DEFAULT current_timestamp(),
   `lomba_update_time` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `lomba_kat_id` int(11) NOT NULL,
@@ -48,12 +87,9 @@ CREATE TABLE `lomba` (
 -- Dumping data for table `lomba`
 --
 
-INSERT INTO `lomba` (`lomba_id`, `lomba_judul`, `lomba_gambar`, `lomba_isi`, `lomba_link`, `lomba_deadline`, `lomba_tiket`, `lomba_created_time`, `lomba_update_time`, `lomba_kat_id`, `lomba_created_by_id`, `lomba_update_by_id`, `status`) VALUES
-(1, '1', '1', '1', '1', '2019-07-09 00:00:18', 'Gratis', '2019-07-09 21:04:25', '2019-07-17 00:00:00', 1, 1, 1, 'Diterima'),
-(2, '', '', '', '', '0000-00-00 00:00:00', 'Gratis', '2019-07-10 15:32:18', '2019-07-10 15:32:18', 0, 0, 0, 'Diterima'),
-(3, '123', '', '', '', '0000-00-00 00:00:00', 'Gratis', '2019-07-10 15:53:41', '2019-07-10 15:53:41', 0, 0, 0, 'Diterima'),
-(4, '123', '', '', '', '0000-00-00 00:00:00', 'Gratis', '2019-07-10 15:54:33', '2019-07-10 15:54:33', 0, 0, 0, 'Diterima'),
-(5, '123', '', '', '', '0000-00-00 00:00:00', 'Gratis', '2019-07-10 15:55:52', '2019-07-10 17:12:14', 1, 0, 0, 'Menunggu');
+INSERT INTO `lomba` (`lomba_id`, `lomba_judul`, `lomba_gambar`, `lomba_isi`, `lomba_link`, `lomba_deadline`, `lomba_tiket`, `lomba_alamat`, `lomba_created_time`, `lomba_update_time`, `lomba_kat_id`, `lomba_created_by_id`, `lomba_update_by_id`, `status`) VALUES
+(1, 'ini adalah dunia yang telah kita dia aku kamu situ sana dan yang ada di sini pohon 1 dot png satu satu', 'pohon1.png', 'ini adalah dunia yang telah kita dia aku kamu situ sana dan yang ada di sini pohon 1 dot png satu satu', '1', '2019-07-09 00:00:18', 'Gratis', 'jl. in aja', '2019-07-09 21:04:25', '2019-07-12 20:29:11', 1, 1, 1, 'Diterima'),
+(14, '', '', '', '', '0000-00-00 00:00:00', '', '', '2019-07-12 06:03:09', '2019-07-12 06:03:09', 0, 1, 1, 'Menunggu');
 
 -- --------------------------------------------------------
 
@@ -96,7 +132,11 @@ CREATE TABLE `team` (
 --
 
 INSERT INTO `team` (`team_id`, `team_nama`, `team_jabatan`, `team_isi`, `team_image`, `team_created_time`, `team_created_by`, `team_update_time`, `team_update_by`) VALUES
-(1, 'Aripsssss', 'gamerssssss', 'mntep nih gw anak usu lohsssssssss', 'python2.jpg', '2018-12-03 14:07:45', 0, '0000-00-00 00:00:00', 0);
+(1, 'Aripsssss', 'gamerssssss', 'mntep nih gw anak usu lohsssssssss', 'contoh_pas_photo.jpg', '2018-12-03 14:07:45', 0, '0000-00-00 00:00:00', 0),
+(2, '1', '1', '1', 'contoh_pas_photo.jpg', '0000-00-00 00:00:00', 1, '0000-00-00 00:00:00', 1),
+(3, '3', '3', '3', 'pohon2.png', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
+(4, '1', '1', '1', 'pohon3.png', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
+(5, '4', '4', '4', 'pohon.png', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0);
 
 -- --------------------------------------------------------
 
@@ -109,19 +149,32 @@ CREATE TABLE `user` (
   `user_name` varchar(255) NOT NULL,
   `user_pass` varchar(255) NOT NULL,
   `user_pass_ret` varchar(255) NOT NULL,
-  `user_level` enum('admin','member') NOT NULL
+  `user_level` enum('admin','member') NOT NULL,
+  `biodata_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `user_name`, `user_pass`, `user_pass_ret`, `user_level`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin');
+INSERT INTO `user` (`user_id`, `user_name`, `user_pass`, `user_pass_ret`, `user_level`, `biodata_id`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `biodata`
+--
+ALTER TABLE `biodata`
+  ADD PRIMARY KEY (`biodata_id`);
+
+--
+-- Indexes for table `kontak`
+--
+ALTER TABLE `kontak`
+  ADD PRIMARY KEY (`kontak_id`);
 
 --
 -- Indexes for table `lomba`
@@ -153,10 +206,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `biodata`
+--
+ALTER TABLE `biodata`
+  MODIFY `biodata_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kontak`
+--
+ALTER TABLE `kontak`
+  MODIFY `kontak_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `lomba`
 --
 ALTER TABLE `lomba`
-  MODIFY `lomba_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `lomba_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `lomba_kategori`
@@ -168,7 +233,7 @@ ALTER TABLE `lomba_kategori`
 -- AUTO_INCREMENT for table `team`
 --
 ALTER TABLE `team`
-  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
